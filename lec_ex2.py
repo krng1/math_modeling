28e89b222fcf4d9a46d4383cca2bab32b4725708
krng1 <72023332+krng1@users.noreply.github.com>
2020-09-28 17:38:37 +0200

    послушали лекцию 2

---
+++ lec_ex2.py
@@ -0,0 +1,7 @@
+a = 3
+if a > 5:
+    print('print 5')
+elif a < 2:
+    print('hello 2')
+else:
+    print('Tup0 hello')
---
+++ lec_ex4.py
@@ -0,0 +1,4 @@
+if ((a == 3 and c>0) or (b==5 and c<0)) and c == -1:
+    print('good')
+else:
+    print('bad')
